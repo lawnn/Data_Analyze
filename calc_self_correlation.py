9ef421a4a3f06d635822fcb7f10ff43dd5976ba8
lawn <bloom.all.over.12@gmail.com>
2021-09-17 08:30:28 +0900

    Adjustable correlation plot

--- calc_self_correlation.py
+++ calc_self_correlation.py
@@ -18,21 +18,22 @@ def plot_corrcoef(arr1, arr2, output_dir: str = None, title: str = None, save_fi
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    correlation = np.corrcoef(arr1, arr2)
-    r2 = correlation[0][1] ** 2
+    correlation = np.corrcoef(arr1, arr2)[1, 0]
+    r2 = correlation ** 2
     # グラフ出力.
     if title is None:
-        title = f"IC = {correlation[0][1]:.4f}"
+        title = 'Correlation'
     fig = plt.figure()
     fig.suptitle(title)
     ax = fig.add_subplot(111)
     ax.scatter(arr1, arr2, c="blue", s=20, edgecolors="blue", alpha=0.3)
-    ax.set_xlabel(f"Return")
-    ax.set_ylabel(f"Indicator")
+    ax.set_xlabel(f"Indicator")
+    ax.set_ylabel(f"Return")
     ax.grid(which="major", axis="x", color="gray", alpha=0.5, linestyle="dotted", linewidth=1)
     ax.grid(which="major", axis="y", color="gray", alpha=0.5, linestyle="dotted", linewidth=1)
-    ax.text(0.75, 0.1, f"R**2={r2:.4f}", transform=ax.transAxes)
-    ax.text(0.55, 0.04, f"ProportionCorrect={(correlation[0][1] + 1) / 2 * 100:.2f}%", transform=ax.transAxes)
+    ax.text(0.84, 0.16, f"IC={correlation:.4f}", transform=ax.transAxes)
+    ax.text(0.798, 0.1, f"R**2={r2:.4f}", transform=ax.transAxes)
+    ax.text(0.60, 0.04, f"ProportionCorrect={(correlation + 1) / 2 * 100:.2f}%", transform=ax.transAxes)
     if save_fig:
         plt.savefig(f'{output_dir}/{title}_corrcoef.png')
     plt.show()
